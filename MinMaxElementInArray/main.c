148d956eb2248bb1bf297ab087da95081ce2ecfe
shubhamkumarsingh <www.shubhamsingh6666@gmail.com>
2022-09-16 11:37:16 +0530

    first commit

---
+++ MinMaxElementInArray/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MinMaxElementInArray
+//
+//  Created by Shubham Kumar Singh on 16/09/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello!\n");
+    return 0;
+}
