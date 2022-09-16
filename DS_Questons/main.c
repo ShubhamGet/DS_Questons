0821b11bdded5d5f2b878bea5feb5186029f3a56
shubhamkumarsingh <www.shubhamsingh6666@gmail.com>
2022-09-16 11:08:36 +0530

    Initial Commit

---
+++ DS_Questons/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  DS_Questons
+//
+//  Created by Shubham Kumar Singh on 16/09/22.
+//
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=5;i++)
+    {
+        printf("%d ",i);
+    }
+    printf("\n");
+    
+    printf("Array in revese order\n");
+    for (i=5; i>=1; i--) {
+        printf("%d ",i);
+    }
+}
