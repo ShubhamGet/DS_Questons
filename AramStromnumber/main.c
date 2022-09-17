8849b169c3e665eb31a7c23c6456a8a914ef68e7
shubhamkumarsingh <www.shubhamsingh6666@gmail.com>
2022-09-17 13:51:57 +0530

    third commit

--- AramStromnumber/main.c
+++ AramStromnumber/main.c
@@ -6,9 +6,25 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int num,ld,sum,temp=0;
+    printf("Enter number\n");
+    scanf("%d",&num);
+    temp=num;
+    while (num!=0) {
+        ld=num%10;
+        num=num/10;
+        sum=sum+ld*ld*ld;
+        
+        if(temp==sum)
+        {
+            printf("Number %d is palindrome ",sum);
+        }
+        else
+        {
+            printf("Number %d isn't palindrome ",sum);
+        }
+    }
     return 0;
 }
